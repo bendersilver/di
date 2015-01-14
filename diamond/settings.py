64f2beccc7df1c60011e8c20ceee04e98c6857df
Кирилл <bendersilver@gmail.com>
2015-01-15 00:12:19 +0500

    доступ

--- diamond/settings.py
+++ diamond/settings.py
@@ -107,7 +107,7 @@ STATICFILES_DIRS = (
     # ("js", os.path.join(BASE_DIR, 'www/static/js')),
     # ("img", os.path.join(BASE_DIR, 'www/static/img')),
 )
-
+SITE_ID = 1
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
